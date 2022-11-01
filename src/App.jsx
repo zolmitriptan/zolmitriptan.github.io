2e3e7813eaa70a721409e7a77cd89b36e91d0ec1
wwz <wanzhi.wang@thoughtworks.com>
2022-11-01 16:08:01 +0800

    move nav links to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Outlet, Link } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Comment from "./components/comment/Comment";
 import Header from "./components/header/Header";
@@ -9,19 +8,7 @@ function App() {
   return (
     <div>
       <Header />
-      <h1>here is the app</h1>
       <Navbar />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
       <Comment />
       <Footer />
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import {Link, Outlet} from "react-router-dom";
 
 function Navbar() {
   return (
-    <h1>here is navbar section</h1>
+    <div>
+      <h1>here is navbar section</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+      {/* rendering child components (of app): home/blog/nopage */}
+    </div>
   );
 }
 
