42cd2b31cf0b7e9a81ac6149bf78ea2dd998ee89
wwz <wanzhi.wang@thoughtworks.com>
2022-11-01 15:58:34 +0800

    add router for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Gallery from "./components/gallery/Gallery";
-import Article from "./components/article/Article";
 import Comment from "./components/comment/Comment";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>here is the app</h1>
       <Navbar />
-      <Gallery />
-      <Article />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
       <Comment />
+      <Footer />
     </div>
   );
 }
+/* default layout section */
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Loading from "./components/loading/Loading";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import NoPage from "./pages/NoPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div>
-      <App />
-    </div>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
 );
