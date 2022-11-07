2e1b471070690d5a73b74a235b8d8253803e049b
wwz <wanzhi.wang@thoughtworks.com>
2022-11-07 14:32:45 +0800

    edit header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,29 @@
 import { Layout } from 'antd';
 import './Header.scss'
+import {Link} from "react-router-dom";
+import React from "react";
 
 function Header() {
   return (
     <Layout.Header className="page-header">
-      <div className="headerLeft">left</div>
-      <div className="headerCenter">center</div>
-      <div className="headerRight">right</div>
+      <div className="headerLeft">
+        <img src="https://img.icons8.com/windows/64/null/007-logo.png" alt="logo"/>
+        <p>left</p>
+      </div>
+      <div className="headerCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link to="/">HOME</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/blogs">BLOGS</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/search">SEARCH</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="headerRight"><p>right</p></div>
     </Layout.Header>
   );
 }
